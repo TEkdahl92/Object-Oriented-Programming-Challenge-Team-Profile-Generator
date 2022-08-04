2db48fe1c85c969476323427c65c2597366ec095
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-08-03 20:48:24 -0500

    created engineer test and card js

---
+++ cards/engineer.js
@@ -0,0 +1,19 @@
+const Employee = require('./employee');
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email, github);
+        this.github = github;
+    };
+
+    getGithub() {
+        console.log(this.github);
+        return this.github;
+    };
+    
+    getRole() {
+        return 'Engineer'
+    };
+}
+
+module.exports = Engineer
---
+++ tests/engineerTest.js
@@ -0,0 +1,14 @@
+const Engineer = require('../cards/engineer');
+
+describe ("Engineer", () => {
+    describe("Initialization", () => {
+        it("should create an object with a name, id, email, and github if provided valid arguments", () => {
+            const engineer = new Engineer('Troy', 123456, 'alex@email.com', 'troygit');
+
+            expect(engineer.name).toEqual('Troy');
+            expect(engineer.id).toEqual(123456);
+            expect(engineer.email).toEqual('Troy@email.com');
+            expect(engineer.github).toEqual('Troygit');
+        })      
+    })
+});
